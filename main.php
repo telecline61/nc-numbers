39c89d6b52b5298967265d7d13b99284948af93f
Chris Cline <chriscline@Chriss-MacBook-Pro-2.local>
2018-01-09 17:46:43 -0800

    initial commit

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: Numbers Control
- * Description: Allows central control of statistics numbers that change often on pages to be controled/updated globally in a CPT format
+ * Plugin Name: Parts List Numbers
+ * Description: Adds custom metabox and field to products for tying parts to parts diagram. Also adds option page for setting IDs of pages that will display the numbers.
  * Version: 0.1
  * Author: Chris Cline
  * Author URI: http://www.christiancline.com
@@ -10,104 +10,54 @@
 // Exit if this wasn't accessed via WordPress (aka via direct access)
 if (!defined('ABSPATH')) exit;
 
-class NcNumbersPlugin {
+class PartNumbersPlugin {
     public function __construct() {
-
         //add metabox and save data
         add_action('add_meta_boxes', array($this, 'add_box'));
         add_action('save_post', array($this, 'save'));
+        //display parts number before title
 
-        // Add CSS - if needed
+        add_action('woocommerce_before_shop_loop_item_title', array($this, 'display_number'));
+        // add options Page
+        add_action( 'admin_menu', array( $this, 'add_page_id_options' ) );
+		add_action( 'admin_init', array( $this, 'page_id_options_init' ) );
+        // add stylesheet
         add_action('wp_enqueue_scripts', array($this,'enqueue'));
-
-        //register the post type
-        add_action('init', array($this,'registration'));
-
-        //add the shortcode
-        add_shortcode('nc_number', array($this,'shortcode'));
     }
-
-    public function registration() {
-        register_post_type(
-            'nc-number',
-            array(
-                'labels' => array(
-                    'name' => _x('NC Number', 'post type general name'),
-                    'singular_name' => _x('NC Number', 'post type singular name'),
-                    'add_new' => _x('Add NC Number', 'nc number'),
-                    'add_new_item' => __('Add New NC Number'),
-                    'edit_item' => __('Edit Number'),
-                    'new_item' => __('New NC Number'),
-                    'all_items' => __('All NC Numbers'),
-                    'view_item' => __('View NC Number'),
-                    'search_items' => __('Search NC Numbers'),
-                    'not_found' =>  __('No NC Numbers'),
-                    'not_found_in_trash' => __('No NC Numbers found in Trash'),
-                    'parent_item_colon' => '',
-                    'menu_name' => 'NC Numbers'
-                ),
-                'show_ui' => true,
-                'show_in_menu' => true,
-                'query_var' => false,
-                'rewrite' => true,
-                'publicly_queryable' => false,
-                'exclude_from_search' => true,
-                'capability_type' => 'post',
-                'has_archive' => false,
-                'hierarchical' => false,
-                'menu_position' => 10,
-                'supports' => array('title'),
-                'public' => true,
-            )
-        );
-    }
-
     // add metabox
     public function add_box() {
-
-    $post_type = 'nc-number';
-        //limit meta box to certain post types
-        add_meta_box('nc-number-metabox',
-        'NC Number',
+        //limit meta box to 'product' post types
+        $post_type = 'product';
+        add_meta_box('parts-number-metabox',
+        'Parts List Number',
         array($this, 'meta_box_function'),
         $post_type,
         'normal',
-        'high');
+        'low');
     }
-
     //render number on metaboxes template
     public function meta_box_function($post) {
         // Add an nonce field so we can check for it later.
-        wp_nonce_field('nc_number_check', 'nc_number_check_value');
-
-        $myValue = get_post_meta($post -> ID, '_nc_number', true);
-        $myId = get_the_ID();
-
+        wp_nonce_field('parts_number_check', 'parts_number_check_value');
+        $myValue = get_post_meta($post -> ID, '_parts_number', true);
         //metabox template
         include( plugin_dir_path( __FILE__ ) . 'templates/metaboxes.php');
-
     }
-
     public function save($post_id) {
-
         // Check if our nonce is set.
-        if (!isset($_POST['nc_number_check_value']))
+        if (!isset($_POST['parts_number_check_value']))
         return $post_id;
 
-        $nonce = $_POST['nc_number_check_value'];
-
+        $nonce = $_POST['parts_number_check_value'];
         // Verify that the nonce is valid.
-        if (!wp_verify_nonce($nonce, 'nc_number_check'))
+        if (!wp_verify_nonce($nonce, 'parts_number_check'))
         return $post_id;
-
         // If this is an autosave, our form has not been submitted,
-        //     so we don't want to do anything.
+        // so we don't want to do anything.
         if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
         return $post_id;
-
         // Check the user's permissions.
         if ('page' == $_POST['post_type']) {
-
         if (!current_user_can('edit_page', $post_id))
         return $post_id;
 
@@ -116,56 +66,105 @@ class NcNumbersPlugin {
         if (!current_user_can('edit_post', $post_id))
         return $post_id;
         }
-
         /* OK, its safe for us to save the data now. */
-
         // Sanitize the user input.
-        $data = sanitize_text_field($_POST['nc_number']);
+        $data = sanitize_text_field($_POST['parts_number']);
 
         // Update the meta field.
-        update_post_meta($post_id, '_nc_number', $data);
+        update_post_meta($post_id, '_parts_number', $data);
     }
-
-    // shortcode function
-    public function shortcode($atts) {
-
-        //class for number span
-        $myClass = isset($atts['class'])  ? $atts['class']  : '';
-        //post ID
-        $myId = isset($atts['id'])  ?  sanitize_text_field($atts['id'])  : $atts['id'] = '1';
-
-    	//query args
-    	$args = array(
-    		 'post_type' => 'nc-number',
-    		 'p' => $atts['id'],
-    		 'posts_per_page' => '1',
-    	 );
-
-         // The Query
-         $the_query = new WP_Query( $args );
-
-         // The Loop get rid of twig
-         if ( $the_query->have_posts() ) {
-
-         	while ( $the_query->have_posts() ) {
-
-                $output = '';
-         		$the_query->the_post();
-
-         	   return $output . '<span class="' . $myClass . '">' . get_post_meta( get_the_ID(), '_nc_number', true ) . '</span>';
-         	}
-
-         	/* Restore original Post Data */
-         	wp_reset_postdata();
-         }
-
+    //display the number in the product loop
+    public function display_number(){
+        //get the comma-separated page ids from our options page
+        $page_ids_options = get_option( 'page_id_option_name' ); // Array of all options
+        $page_ids_0 = $page_ids_options['page_ids_0']; //comma separated string of our page ids
+        // convert to int. array
+        $myString = $page_ids_0;
+        $myArray = explode(',', $myString);
+        //if pages are in the array display the parts numbers
+        if (is_page($myArray)) {
+    	global $post;
+    	$part_number = get_post_meta($post->ID, '_parts_number', true);
+            //only display if not empty
+            if($part_number){
+                echo'<span class="part-no">'.$part_number.'</span>';
+            }
+        }
     }
+    // build our options page
+    public function add_page_id_options() {
+		add_menu_page(
+			'Parts list Page IDs', // page_title
+			'Parts List Page IDs', // menu_title
+			'manage_options', // capability
+			'parts-list-page-ids', // menu_slug
+			array( $this, 'create_admin_page' ), // function
+			'dashicons-admin-generic', // icon_url
+			100 // position
+		);
+	}
+	public function create_admin_page() {
+		$this->page_ids_options = get_option( 'page_id_option_name' ); ?>
 
+		<div class="wrap">
+			<h2>Parts List Page IDs</h2>
+			<p>Add comma-separated page ids for the pages you want to display the products with parts numbers on.</p>
+			<?php settings_errors(); ?>
+
+			<form method="post" action="options.php">
+				<?php
+					settings_fields( 'page_id_option_group' );
+					do_settings_sections( 'page-ids-admin' );
+					submit_button();
+				?>
+			</form>
+		</div>
+	<?php }
+
+	public function page_id_options_init() {
+		register_setting(
+			'page_id_option_group', // option_group
+			'page_id_option_name', // option_name
+			array( $this, 'sanitize' ) // sanitize_callback
+		);
+		add_settings_section(
+			'page_ids_setting_section', // id
+			'', // title
+			array( $this, 'page_ids_setting_section_info' ), // callback
+			'page-ids-admin' // page
+		);
+		add_settings_field(
+			'page_ids_0', // id
+			'Comma-separated page IDs:', // title
+			array( $this, 'page_ids_0_callback' ), // callback
+			'page-ids-admin', // page
+			'page_ids_setting_section' // section
+		);
+	}
+	public function sanitize($input) {
+		$sanitary_values = array();
+		if ( isset( $input['page_ids_0'] ) ) {
+			$sanitary_values['page_ids_0'] = sanitize_text_field( $input['page_ids_0'] );
+		}
+
+		return $sanitary_values;
+	}
+	public function page_ids_setting_section_info() {
+        echo 'EXAMPLE: 123,456,1780 etc.';
+    }
+	public function page_ids_0_callback() {
+		printf(
+			'<input class="regular-text" type="text" name="page_id_option_name[page_ids_0]" id="page_ids_0" value="%s">',
+			isset( $this->page_ids_options['page_ids_0'] ) ? esc_attr( $this->page_ids_options['page_ids_0']) : ''
+		);
+	}
     // enqueue function for nc numbers css - if needed
-    public function enqueue() {
-        wp_enqueue_style('nc-numbers', plugins_url('css/nc-numbers.css', __FILE__), null, '1.0');
-    }
+   public function enqueue() {
+       //make this load after woocomm styles
+       $woo_deps = class_exists( 'WooCommerce' ) ? array( 'woocommerce-layout', 'woocommerce-smallscreen', 'woocommerce-general' ) : array();
 
+       wp_enqueue_style('part-numbers', plugins_url('css/part-numbers.css', __FILE__), $woo_deps, '1.0');
+   }
 }
 // Let's do this thing!
-$ncNumPlug = new  NcNumbersPlugin();
+$partNumPlug = new  PartNumbersPlugin();
--- templates/metaboxes.php
+++ templates/metaboxes.php
@@ -1,9 +1,10 @@
 <!-- Display the form using the current value. -->
-<div class="numbers-wrapper" style="margin:0; padding-top:0;text-align:left">
-    <label for="nc_number" style="cursor: inherit;">
-        <strong><p>Add Number</p></strong>
-        <p>Add your number here. You can then call this number anywhere in the site with a shortcode. Numbers can be in any format e.g. $123.00, 12,345 12345 etc.</p>
-        <p>Code for this number: <span>[nc_number id="<?php echo $myId; ?>"]</span></p>
+<div class="product_custom_field">
+    <p class="form-field _custom_product_text_field_field ">
+    <label for="parts_number" style="cursor: inherit;">
+        <p>The number you enter here will correspond to the number on the parts diagram.</p>
+        <p><i>Note:</i> This field will be empty for any product that is not represented on the parts diagram(s).</p>
     </label>
-    <input type="text" id="" class="" name="nc_number" value="<?php echo $myValue; ?>" placeholder="" style="width: 100%;margin:0;height: 28px;">
+    <input type="text" id="" class="" name="parts_number" value="<?php echo $myValue; ?>" placeholder="" style="margin:0;height: 28px;">
+    </p>
 </div>
